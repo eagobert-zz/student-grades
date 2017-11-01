fb6e3147ba2795e27410c4190570a6c5dd0cfa4d
eagobert <eagobert@hotmail.com>
2017-11-01 15:47:15 -0500

    Update exercise

--- grades.js
+++ grades.js
@@ -44,7 +44,7 @@ for (let i = 0; i < scores.length; i++) {
     if (scores[i] >= 61 && scores[i] <= 70) {
         gradeD.push(scores[i]);
     }
-    
+
     if (scores[i] <= 60){
         gradeF.push(scores[i]);
     }
@@ -53,22 +53,27 @@ for (let i = 0; i < scores.length; i++) {
 // How many of each grade? Accomplish this with a for..in loop.
 for (let key in gradesDatabase) {
 
-    //This code logs to console the key and key values for each grade range
-    console.log(key + ": " + gradesDatabase[key]);
+    //This code logs to console the key and count of key values for each grade range
+    console.log("There are " + gradesDatabase[key].length + " " + key + "'s in the grade database");
 
-    //This code logs to console the key and # of key values for each grade range
-    console.log(key + ": " + gradesDatabase[key].length);
+    // This code logs to console the min grade for each grade range using the compare function to ensure sort order
+    let minGrade = Math.min.apply(null, gradesDatabase[key])
+    console.log(minGrade + " is the lowest " + key + " grade");
 
-    // This code logs to console the key and sorted key values for each grade range
-    console.log(key + ": " + gradesDatabase[key].sort());
+    // This code logs to console the max grade for each grade range using the compare function to ensure sort order
+    let maxGrade = Math.max.apply(null, gradesDatabase[key])
+    console.log(maxGrade + " is the highest " + key + " grade");
 
-}
-
-
-// What is the lowest score? Sort the array and find out.
-
-// What is the highest score?
+} 
 
 // Which grade had the most students achieve it? Use an if statment, and a currentGradeCount variable, in your for..in loop to see if the current grade's value is higher than the last one.
+
+    //Step 1: I want to create an array from each gradeDatabase[key] length
+    let currentGradeCount = [gradesDatabase["A"].length, gradesDatabase["B"].length, gradesDatabase["C"].length, gradesDatabase["D"].length, gradesDatabase["F"].length
+    ];
+    
+    //Step 2:  I want to find the max number of each key length OR sort and split into groups
+    let maxGradeCount = Math.max.apply(null, currentGradeCount)
+    //Step 3:  I want to log it to the console
 
 // Which grade had the fewest students achieve it?
